1785955b125e57781593c3d427364669ccb8f19a
Viachaslau Lapitski <Viachaslau_Lapitski@epam.com>
2018-02-13 15:39:59 +0300

    add nunit infrastructure

---
+++ DSA/StartupTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DSA
+{
+    [TestFixture]
+    public class StartupTest
+    {
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Startup test");
+        }
+    }
+}
